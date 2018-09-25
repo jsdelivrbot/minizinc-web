45a649259dd868cf0eeda9d53b37353d98400d4a
Harry <hsthomas@verisk.com>
2018-09-25 17:15:10 -0600

    ace code editor mostly functional

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>Here is my app!</h1>
+    <editor id="editor" v-model="codeEntered" lang="html" theme="twilight" mode="javascript" @init="initEditor"></editor>
   </div>
 </template>
 
@@ -8,7 +8,22 @@
 
 export default {
   name: 'app',
+  data: function(){
+    return{
+      codeEntered:""
+    }
+  },
   components: {
+    editor: require('vue2-ace-editor')
+  },
+  methods:{
+    initEditor(){
+
+      require('brace/mode/javascript');
+      require('brace/theme/twilight');
+    }
+  },
+  created(){
   }
 }
 </script>
@@ -21,5 +36,20 @@ export default {
 	text-align: center;
 	color: #2c3e50;
 	margin-top: 60px;
+}
+
+body {
+	overflow: hidden;
+	background-color: black;
+}
+
+#editor {
+	margin: 0;
+	margin-top: 10px;
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
 }
 </style>
