92506fa787180b410cff4dd917203a8346f9f960
Harry <hsthomas@verisk.com>
2018-11-01 22:35:52 -0600

    slight adjustments

--- frontend/src/components/ide.vue
+++ frontend/src/components/ide.vue
@@ -206,37 +206,39 @@ body {
           <v-layout align-space-around>
             <v-flex xs8 fill-height fill-width>
               <editor id="editor" v-model="codeEntered" lang="ruby" v-bind:theme="theme" @change="saveSelectedFile"></editor>
-              <v-dialog light v-model="showShareProject" max-width="500px">
-                <v-card>
-                  <v-card-title class="share-project-title">
-                    Share "{{selectedProject.name}}"
-                  </v-card-title>
-                    <v-list>
-                      <div v-if="selectedProject.collaborators.length > 0" >
-                        <v-flex xs9>
-                        <v-list-tile v-for="(collaborator, index) in selectedProject.collaborators" :key="collaborator" >
-                          <v-list-tile-title>{{collaborator}}</v-list-tile-title>
-                          <v-btn fab flat small v-on:click="removeCollaborator(index)">
-                            <v-icon dark>delete</v-icon>
-                          </v-btn>
+              <div v-if="projectsExist">
+                <v-dialog light v-model="showShareProject" max-width="500px">
+                  <v-card>
+                    <v-card-title class="share-project-title">
+                      Share "{{selectedProject.name}}"
+                    </v-card-title>
+                      <v-list>
+                        <div v-if="selectedProject.collaborators.length > 0" >
+                          <v-flex xs9>
+                            <v-list-tile v-for="(collaborator, index) in selectedProject.collaborators" :key="collaborator" >
+                              <v-list-tile-title>{{collaborator}}</v-list-tile-title>
+                              <v-btn fab flat small v-on:click="removeCollaborator(index)">
+                                <v-icon dark>delete</v-icon>
+                              </v-btn>
+                            </v-list-tile>
+                          </v-flex>
+                        </div>
+                        <v-list-tile>
+                          <v-list-tile-title v-if="selectedProject.collaborators.length <= 0">No collaborators invited. Invited one!</v-list-tile-title>
                         </v-list-tile>
+                      </v-list>
+                      <v-layout>
+                        <v-flex xs10 >
+                          <v-text-field class="add-collaborator" max-width="300px" label="New Collaborator's Email address" @keyup.enter="addCollaborator" v-model="newCollaboratorEmail"
+                            autofocus light></v-text-field>
                         </v-flex>
-                      </div>
-                      <v-list-tile>
-                        <v-list-tile-title v-if="selectedProject.collaborators.length <= 0">No collaborators invited. Invited one!</v-list-tile-title>
-                      </v-list-tile>
-                    </v-list>
-                    <v-layout>
-                      <v-flex xs10 >
-                        <v-text-field class="add-collaborator" max-width="300px" label="New Collaborator's Email address" @keyup.enter="addCollaborator" v-model="newCollaboratorEmail"
-                          autofocus light></v-text-field>
-                      </v-flex>
-                    </v-layout>
-                  <v-card-actions>
-                    <v-btn color="error" @click="showShareProject=false">Done</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
+                      </v-layout>
+                    <v-card-actions>
+                      <v-btn color="error" @click="showShareProject=false">Done</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </div>
             </v-flex>
             <v-flex xs4 fill-height fill-width>
               <v-layout>
